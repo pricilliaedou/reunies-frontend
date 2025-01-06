7458185d0370108a9ba2044d04f895f3f3965084
pricillia <pricillia.edou@gmail.com>
2025-01-06 15:23:38 +0100

    vérification du token dans reseautage pour empecher l'accès directement via l'url

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { useState } from "react";
 import Cookies from "js-cookie";
 
@@ -12,12 +17,12 @@ import Erreur from "./routes/Erreur";
 
 // Composants
 import Layout from "./composants/Layout";
-
 import "./index.css";
 
 function App() {
   const [token, setToken] = useState(Cookies.get("token") || null);
   const [username, setUsername] = useState(Cookies.get("username") || null);
+
   const setUser = (data) => {
     if (data) {
       console.log(data);
@@ -34,35 +39,7 @@ function App() {
       Cookies.remove("username");
     }
   };
-  const activites = [
-    "Informatique",
-    "Santé",
-    "Commerce",
-    "Éducation",
-    "Transport",
-    "Tourisme",
-  ];
 
-  const regions = [
-    "Auvergne-Rhône-Alpes",
-    "Bourgogne-Franche-Comté",
-    "Bretagne",
-    "Centre-Val de Loire",
-    "Corse",
-    "Grand Est",
-    "Hauts-de-France",
-    "Île-de-France",
-    "Normandie",
-    "Nouvelle-Aquitaine",
-    "Occitanie",
-    "Pays de la Loire",
-    "Provence-Alpes-Côte d’Azur",
-    "Guadeloupe",
-    "Martinique",
-    "Guyane",
-    "La Réunion",
-    "Mayotte",
-  ];
   return (
     <Router>
       <div className='container mx-auto max-w-[1280px]'></div>
@@ -81,7 +58,7 @@ function App() {
                   <p className='mt-4 text-3xl font-bold'>au Féminin</p>
                   <button
                     onClick={() => (window.location.href = "/connexion")}
-                    className='bg-[#fdcd23] text-[#ffffff] px-8 py-2 rounded mt-6 font-bold'
+                    className='bg-[#fdcd23] text-[#ffffff] px-8 py-2 rounded mt-6 font-bold hover:bg-[#e6b91f]'
                   >
                     Réseauter
                   </button>
@@ -129,8 +106,6 @@ function App() {
                 setUser={setUser}
                 token={token}
                 username={username}
-                activites={activites}
-                regions={regions}
               />
             </Layout>
           }
@@ -138,24 +113,28 @@ function App() {
         <Route
           path='/reseautage'
           element={
-            <Layout
-              dynamicContent={
-                <>
-                  <h1 className='text-4xl md:text-5xl font-bold text-[#fdcd23]'>
-                    Réunies
-                  </h1>
-                  <p className='mt-4 text-3xl font-bold'>
-                    pour Entreprendre au Féminin
-                  </p>
-                  <p className='mt-4 text-3xl font-bold'>au Féminin</p>
-                </>
-              }
-              setUser={setUser}
-              token={token}
-              username={username}
-            >
-              <Reseautage activites={activites} regions={regions} />
-            </Layout>
+            token ? (
+              <Layout
+                dynamicContent={
+                  <>
+                    <h1 className='text-4xl md:text-5xl font-bold text-[#fdcd23]'>
+                      Réunies
+                    </h1>
+                    <p className='mt-4 text-3xl font-bold'>
+                      pour Entreprendre au Féminin
+                    </p>
+                    <p className='mt-4 text-3xl font-bold'>au Féminin</p>
+                  </>
+                }
+                setUser={setUser}
+                token={token}
+                username={username}
+              >
+                <Reseautage />
+              </Layout>
+            ) : (
+              <Navigate to='/connexion' />
+            )
           }
         />
         <Route path='/succes' element={<Succes />} />
